6ecf863bb8878c433f1221cd1b901c19f3845b03
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-10-25 12:56:55 +0100

    try coveralls

---
+++ test/Errors.ts
@@ -0,0 +1,36 @@
+import test from 'ava';
+import {LazyGetter} from "../lib/LazyGetter";
+import * as tslib from 'tslib';
+
+test('Not a getter', t => {
+  t.throws(() => {
+    class Test {
+
+      @LazyGetter()
+      notAGetter() {
+      }
+    }
+  }, (e: Error) => e.message === '@LazyGetter can only decorate getters!')
+});
+
+test('Not a getter', t => {
+  t.throws(() => {
+    class Test {
+
+    }
+
+    Object.defineProperty(Test.prototype, 'foo', {
+      configurable: false,
+      get() {
+        return 1;
+      }
+    });
+
+    tslib.__decorate(
+      [LazyGetter()],
+      Test.prototype,
+      'foo',
+      null
+    );
+  }, (e: Error) => e.message === '@LazyGetter target must be configurable')
+});
