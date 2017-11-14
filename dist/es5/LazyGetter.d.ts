4ee3848dab20425e51783413608a2f9af311f9ed
Arturas Molcanovas <a.molcanovas@gmail.com>
2017-11-14 18:17:12 +0000

    rm dist folder

--- dist/es5/LazyGetter.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Evaluate the getter function and cache the result
- * @param {boolean} [setProto=false] Set the value on the class prototype as well. Only applies to non-static getters.
- * @param {boolean} [makeNonConfigurable=false] Set to true to make the resolved property non-configurable
- * @return {(target: any, key: string, descriptor: PropertyDescriptor) => void} A Typescript decorator function
- */
-export declare function LazyGetter(setProto?: boolean, makeNonConfigurable?: boolean): (target: any, key: string, descriptor: PropertyDescriptor) => void;
