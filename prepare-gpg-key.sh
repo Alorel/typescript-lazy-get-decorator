38c32aa237c2ca25b9771c5b6c331c954a3ce55c
Arturas <a.molcanovas@gmail.com>
2019-07-10 20:30:19 +0100

    ci: Stop signing release commits
    Closes #105, closes #108, closes #109

--- prepare-gpg-key.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-openssl aes-256-cbc -K $encrypted_c30c4348ae12_key -iv $encrypted_c30c4348ae12_iv -in git_gpg_keys.asc.enc -out /tmp/git_gpg_keys.asc -d
-chmod 600 /tmp/git_gpg_keys.asc
-gpg --batch --yes --import /tmp/git_gpg_keys.asc
-echo '/usr/bin/gpg2 --passphrase ${GPG_PASSPHRASE} --batch --no-tty "$@"' > /tmp/gpg-with-passphrase && chmod +x /tmp/gpg-with-passphrase
-git config gpg.program "/tmp/gpg-with-passphrase"
-git config commit.gpgsign true
-git config --global user.signingkey ${GPG_KEY_ID}
